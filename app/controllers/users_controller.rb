f389ff2cdd8176cf11d65c17a0726abbb0ed7822
Honda <hondao1207@gmail.com>
2020-12-10 08:46:27 +0900

    financial-tracker

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,31 @@
+class UsersController < ApplicationController
+    def myportfolio
+        @tracked_stocks = current_user.stocks
+    end 
+
+    def my_friends
+        @friends = current_user.friends
+    end
+
+    def search
+        if params[:friend].present?
+             @friends = User.search(params[:friend])
+             @friends = current_user.except_current_user(@friends)
+            if @friends
+                respond_to do | format |
+                  format.js { render partial: 'users/friend_result'}
+                end
+            else
+              respond_to do | format |
+               flash.now[:alert] = "cant find user"
+               format.js { render partial: 'users/friend_result' }
+              end
+            end
+        else 
+          respond_to do | format |
+             flash.now[:alert] = "Please enter a symbol to search"
+             format.js { render partial: 'users/friend_result' }
+          end
+        end
+     end
+end
